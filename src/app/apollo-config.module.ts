b5fd43fd408dc042eb87971d89ceeb010e5e1cfd
Andre Machida <andmachida@gmail.com>
2018-11-27 23:48:43 -0200

    Hadling errors with apolloLinkError

--- src/app/apollo-config.module.ts
+++ src/app/apollo-config.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { ApolloModule, Apollo } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 
 @NgModule({
   imports: [
@@ -21,8 +23,24 @@ export class ApolloConfigModule {
     const uri = 'https://api.graph.cool/simple/v1/cjp0chfflkxk80156e729lkcw';
     const http = httpLink.create({ uri });
 
+    const linkError = onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      }
+      if (networkError) {
+        console.log(`[Network error]: ${networkError}`);
+      }
+    });
+
     apollo.create({
-      link: http,
+      link: ApolloLink.from([
+        linkError,
+        http
+      ]),
       cache: new InMemoryCache()
     });
   }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
   constructor(
     private apollo: Apollo
   ) {
-    this.createUser();
+    // this.createUser();
     this.allUsers();
   }
 
