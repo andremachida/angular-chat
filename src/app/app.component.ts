64145c85ef2ca46ea7af75e5315a96d3a5ae2653
Andre Machida <andmachida@gmail.com>
2018-11-27 22:34:35 -0200

    Calling graphcool api using httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-chat';
+
+  private apiUrl = 'https://api.graph.cool/simple/v1/cjp0chfflkxk80156e729lkcw';
+
+  constructor(
+    private http: HttpClient
+  ) {
+    // this.createUser();
+    this.allUsers();
+  }
+
+  allUsers(): void {
+    const body = {
+      query: `
+        query {
+          allUsers {
+            id
+            name
+            email
+          }
+        }
+      `
+    };
+
+    this.http.post(this.apiUrl, body)
+      .subscribe(res => console.log('Query: ', res));
+  }
+
+  createUser(): void {
+    const body = {
+      query: `
+        mutation CreateNewUser($name: String!, $email: String!, $password: String!) {
+          createUser (
+            name: $name,
+            email: $email,
+            password: $password
+          ) {
+            id
+            name
+            email
+          }
+        }
+      `,
+      variables: {
+        name: 'Annelise',
+        email: 'anne@gmail.com',
+        password: '123'
+      }
+    };
+
+    this.http.post(this.apiUrl, body)
+      .subscribe(res => console.log('Mutation: ', res));
+  }
 }
