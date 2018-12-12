c00c1d76a2bb5efc32c04130972d067a9a6b7ef8
Andre Machida <andmachida@gmail.com>
2018-12-12 21:26:52 -0200

    Created header component; Implemented logout;

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,16 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { AppRoutingModule } from '../app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ApolloConfigModule } from '../apollo-config.module';
+import { Title } from '@angular/platform-browser';
 
 @NgModule({
   exports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     ApolloConfigModule
+  ],
+  providers: [
+    Title
   ]
 })
 export class CoreModule {
