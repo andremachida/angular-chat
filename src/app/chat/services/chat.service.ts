8fddea716fbc04f0fbd21815cec0851892660457
Andre Machida <andmachida@gmail.com>
2018-12-14 22:25:08 -0200

    Created getChat query and service;

--- src/app/chat/services/chat.service.ts
+++ src/app/chat/services/chat.service.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 import { Chat } from '../models/chat.model';
 import { Apollo } from 'apollo-angular';
 import { AuthService } from '../../core/services/auth.service';
-import { AllChatsQuery, USER_CHATS_QUERY } from './chat.graphql';
+import { AllChatsQuery, USER_CHATS_QUERY, ChatQuery, CHAT_BY_ID_OR_BY_USERS_QUERY } from './chat.graphql';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -24,6 +24,19 @@ export class ChatService {
       }
     }).pipe(
       map(res => res.data.allChats)
+    );
+  }
+
+  getChatByIdOrByUsers(chatOrUserId: string): Observable<Chat> {
+    return this.apollo.query<ChatQuery | AllChatsQuery>({
+      query: CHAT_BY_ID_OR_BY_USERS_QUERY,
+      variables: {
+        chatId: chatOrUserId,
+        targetUserId: chatOrUserId,
+        loggedUserId: this.authService.authUser.id
+      }
+    }).pipe(
+      map(res => (res.data['Chat']) ? (res.data['Chat']) : res.data['allChats'][0])
     );
   }
 }
