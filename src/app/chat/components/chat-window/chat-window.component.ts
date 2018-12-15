ba71c3eeb9dcafa2e88f1b27d99b31c4523aff12
Andre Machida <andmachida@gmail.com>
2018-12-14 23:53:30 -0200

    Created chat window resolver;

--- src/app/chat/components/chat-window/chat-window.component.ts
+++ src/app/chat/components/chat-window/chat-window.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Chat } from '../../models/chat.model';
+import { Subscription } from 'rxjs';
+import { map, mergeMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat-window',
   templateUrl: './chat-window.component.html',
   styleUrls: ['./chat-window.component.scss']
 })
-export class ChatWindowComponent implements OnInit {
+export class ChatWindowComponent implements OnInit, OnDestroy {
+  chat: Chat;
+  recipientId: string = null;
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.route.data
+        .pipe(
+          map(routeData => this.chat = routeData.chat),
+          mergeMap(() => this.route.paramMap),
+          tap((params: ParamMap) => {
+            if (this.chat) {
+              this.recipientId = params.get('id');
+              console.log(this.recipientId);
+            }
+          })
+        )
+        .subscribe()
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 
 }
---
+++ src/app/chat/components/chat-window/chat-window.resolver.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Chat } from '../../models/chat.model';
+import { Observable } from 'rxjs';
+import { ChatService } from '../../services/chat.service';
+
+@Injectable()
+export class ChatWindowResolver implements Resolve<Chat> {
+
+  constructor(
+    private chatService: ChatService
+  ) {
+
+  }
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Chat> {
+    const chatOrUserId: string = route.paramMap.get('id');
+    return this.chatService.getChatByIdOrByUsers(chatOrUserId);
+  }
+}
