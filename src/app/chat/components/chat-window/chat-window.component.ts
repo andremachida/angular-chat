43e7857357779ca3499af9cdeed2a54062474d67
Andre Machida <andmachida@gmail.com>
2018-12-15 00:12:12 -0200

    Loading user name or chat title on toolbar;

--- src/app/chat/components/chat-window/chat-window.component.ts
+++ src/app/chat/components/chat-window/chat-window.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Chat } from '../../models/chat.model';
 import { Subscription } from 'rxjs';
-import { map, mergeMap, tap } from 'rxjs/operators';
+import { map, mergeMap, tap, take } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
+import { UserService } from '../../../core/services/user.service';
+import { User } from '../../../core/models/user.model';
 
 @Component({
   selector: 'app-chat-window',
@@ -15,19 +18,26 @@ export class ChatWindowComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription[] = [];
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private title: Title,
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
+    this.title.setTitle('Loading...');
     this.subscriptions.push(
       this.route.data
         .pipe(
           map(routeData => this.chat = routeData.chat),
           mergeMap(() => this.route.paramMap),
           tap((params: ParamMap) => {
-            if (this.chat) {
+            if (!this.chat) {
               this.recipientId = params.get('id');
-              console.log(this.recipientId);
+              this.userService.getUserById(this.recipientId)
+                .pipe(take(1))
+                .subscribe((user: User) => this.title.setTitle(user.name));
+            } else {
+              this.title.setTitle(this.chat.title || this.chat.users[0].name);
             }
           })
         )
@@ -37,6 +47,7 @@ export class ChatWindowComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subscriptions.forEach(s => s.unsubscribe());
+    this.title.setTitle('Angular Chat');
   }
 
 }
